7e6495db2ffb344cbadf2fa7fae8c1e624aacae0
brlaney42 <brlaney@outlook.com>
2021-11-18 10:13:08 -0600

    Adding capitalize function need to move api

--- src/cli.js
+++ src/cli.js
@@ -1,23 +1,62 @@
 import arg from 'arg';
+import inquirer from 'inquirer';
 
 function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
     {
       '--capitalize': Boolean,
-      '--c': '--capitalize',
+      '--yes': Boolean,
+      '--words': Number,
+      '-c': '--capitalize',
+      '-y': '--yes',
+      '-w': '--words',
     },
     {
       argv: rawArgs.slice(2),
     }
   );
+
   return {
+    skipPrompts: args['--yes'] || false,
     capitalize: args['--capitalize'] || false, // default: false
-    words: args._[0] || 3, // default: 3
+    words: args['--words'] || 3, // default: 3
   };
 }
 
-export function cli(args) {
+// async function promptForMissingOptions(options) { 
+//   const defaultCap = false;
+
+//   if (options.skipPrompts) {
+//     return {
+//       ...options,
+//       capitalize: options.capitalize || defaultCap,
+//     };
+//   }
+
+//   const questions = [];
+  
+//   if (!options.capitalize) {
+//     questions.push({
+//       type: 'list',
+//       name: 'capitalize',
+//       message: 'Capitalize the first letter in each word: ',
+//       choices: [
+//         false,
+//         true
+//       ],
+//     })
+//   }
+
+//   const answers = await inquirer.prompt(questions);
+//   return {
+//     ...options,
+//     capitalize: options.capitalize || answers.capitalize,
+//   };
+// }
+
+export async function cli(args) {
   let options = parseArgumentsIntoOptions(args);
+  // options = await promptForMissingOptions(options);
 
   console.log(options);
 }
---
+++ src/utils/lib.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch';
+
+const api_url = 'https://random-word-api.herokuapp.com/word?number=3&swear=0';
+
+/* A reusable function `capitalize`. 
+  Input: string & 
+  Output: string with first letter capitalized. 
+*/
+const capitalize = (str) => {
+  if(typeof str === 'string') {
+      return str.replace(/^\w/, c => c.toUpperCase());
+  } else {
+      return '';
+  }
+};
+
+// NEXT YOU NEED TO MOVE THE getApiData function!
+
+// Async function to fetch API response
+async function getApiData(url) {
+
+  const response = await fetch(url);
+  var data = await response.json(); // Save response in variable data
+
+  if (response) {
+    console.log('Response exists: ')
+  }
+
+  console.log(data);
+}
+
+const newData = getApiData(api_url);
+console.log(newData);
+
+module.exports = {
+  capitalize,
+};
